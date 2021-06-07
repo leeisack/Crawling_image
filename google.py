3456da9e9733bbc0ef94bd4bb9b6986829d0331a
isack Lee <sackda24@naver.com>
2021-06-07 17:57:22 +0900

    crawler

--- google.py
+++ google.py
@@ -1,28 +1,48 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup as soups
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
 
-def search_selenium(search_name, search_path, search_limit) :
-    search_url = "https://www.google.com/search?q=" + str(search_name) + "&hl=ko&tbm=isch"
-    
-    browser = webdriver.Chrome('/usr/bin/chromedriver')
-    browser.get(search_url)
-    
-    image_count = len(browser.find_elements_by_tag_name("img"))
-    
-    print("로드된 이미지 개수 : ", image_count)
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+elem =driver.find_element_by_name("q")
+elem.send_keys("quadrotor drone")
+elem.send_keys(Keys.RETURN)
 
-    browser.implicitly_wait(2)
 
-    for i in range( search_limit ) :
-        image = browser.find_elements_by_tag_name("img")[i]
-        image.screenshot("/home/kitechai/Visual_Studio/venv/image" + str(i) + ".png")
+SCROLL_PAUSE_TIME = 1
 
-    browser.close()
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
 
-if __name__ == "__main__" :
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    search_name = "Drone"
-    search_limit = int(100)
-    search_path = "Your Path"
-    # search_maybe(search_name, search_limit, search_path)
-    search_selenium(search_name, search_path, search_limit)
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except:
+            break
+    last_height = new_height
+
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+for image in images:
+    if count < 2000:
+        try:
+            image.click()
+            time.sleep(2)
+            imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img").get_attribute("src")
+            urllib.request.urlretrieve(imgUrl,str(count)+".jpg")
+            count += 1
+        except:
+            pass
+    else:
+        break
+# driver.close()
